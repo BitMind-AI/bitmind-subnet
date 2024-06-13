b89f1d123d83cae82d5a6b9daedeb466cdd95510
Dylan Uys <dylan.uys@gmail.com>
2024-06-13 09:23:07 +0000

    add proxy arguments

--- bitmind/utils/config.py
+++ bitmind/utils/config.py
@@ -235,6 +235,20 @@ def add_validator_args(cls, parser):
         default="opentensor-dev",
     )
 
+    parser.add_argument(
+        "--proxy.port",
+        type=int,
+        help="The port to run the proxy on.",
+        default=47923, #None
+    )
+
+    parser.add_argument(
+        "--proxy.proxy_client_url",
+        type=str,
+        help="The url initialize credentials for proxy.",
+        default="http://0.0.0.0:47927",
+    )
+
 
 def config(cls):
     """
