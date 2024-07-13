50a27b0d409a314245e36b01d47bde54f032c9d7
Benjamin <caliangben@gmail.com>
2024-07-12 20:52:33 -0400

    Created jupyter notebook for the entire pipeline of synthetic image generation

--- bitmind/synthetic_image_generation/synthetic_image_generator.py
+++ bitmind/synthetic_image_generation/synthetic_image_generator.py
@@ -16,9 +16,7 @@ import bittensor as bt
 from bitmind.constants import PROMPT_GENERATOR_NAMES, PROMPT_GENERATOR_ARGS, DIFFUSER_NAMES, DIFFUSER_ARGS
 
 class SyntheticImageGenerator:
-    def __init__(self, annotations_dir: str, output_dir: str, device: str = 'auto'):
-        self.annotations_dir = annotations_dir
-        self.output_dir = output_dir
+    def __init__(self, device: str = 'auto'):
         self.device = torch.device('cuda' if torch.cuda.is_available() and device == 'auto' else 'cpu')
         if self.device.type == 'cpu':
             raise RuntimeError("This script requires a GPU because it uses torch.float16.")
@@ -29,12 +27,14 @@ class SyntheticImageGenerator:
         logging.basicConfig(level=logging.INFO)
         warnings.filterwarnings("ignore", category=FutureWarning, module='diffusers')
 
-    def generate_image(self, annotation: dict, diffuser: DiffusionPipeline):
-        """ Generate images from annotations using a diffuser and save to the specified directory. """
+    def generate_image(self, annotation: dict, diffuser: DiffusionPipeline, save_dir: str):
+        """Generate images from annotations using a diffuser and save to the specified directory."""
         with torch.no_grad():
             prompt = annotation['description']
-            index = annotation.get('index', f"missing_index")    
+            index = annotation.get('index', "missing_index")
             generated_image = diffuser(prompt=prompt).images[0]
             img = ToPILImage()(generated_image) if isinstance(generated_image, torch.Tensor) else generated_image
-            img_filename = f"{save_dir}/{prompt[:50].replace(' ', '_').replace('/', '_').replace('\\', '_')}-{index}.png"
+            safe_prompt = prompt[:50].replace(' ', '_').replace('/', '_').replace('\\', '\\\\')
+            
+            img_filename = f"{save_dir}/{safe_prompt}-{index}.png"
             return img, img_filename
