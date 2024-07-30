1a50e18ac30d47e8a83a1263edac3e73ae4de68e
Dylan Uys <dylan.uys@gmail.com>
2024-07-29 19:48:54 -0500

    mainnet start scripts

--- start_testnet_validator.sh
+++
@@ -1,1 +0,0 @@
-pm2 start python --name validator -- ./neurons/validator.py --netuid 168 --subtensor.network test --wallet.name default --wallet.hotkey default --axon.port 8091 --neuron.network test --wandb.off
