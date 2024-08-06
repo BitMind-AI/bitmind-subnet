1c7dee4f0c2508be105d0f1c4cadfea8f79dc38d
Benjamin <caliangben@gmail.com>
2024-08-06 12:44:41 -0400

    Update dlib requirements

--- install_system_deps.sh
+++ install_system_deps.sh
@@ -4,4 +4,6 @@ sudo apt install nano -y
 sudo apt install libgl1 -y
 sudo apt install npm -y
 sudo npm install pm2@latest -g 
-
+sudo apt install build-essential cmake -y
+sudo apt install libopenblas-dev liblapack-dev -y
+sudo apt install libx11-dev libgtk-3-dev -y
