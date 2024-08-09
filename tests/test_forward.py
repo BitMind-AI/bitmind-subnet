3a0d940cb94285d0717bf53354d46b4aa0d9327a
Dylan Uys <dylan.uys@gmail.com>
2024-08-08 23:59:10 -0700

    forcing vali fwd through real and synth image flows

--- tests/test_forward.py
+++ tests/test_forward.py
@@ -6,19 +6,19 @@ from bitmind.validator.forward import forward
 
 
 @pytest.mark.asyncio
-async def test_validator_forward():
-    print("Configuring mock config and mock validator")
+@pytest.mark.parametrize("fake_prob", [1., 0.])
+async def test_validator_forward(fake_prob):
+    print(f"Configuring mock config and mock validator, fake_prob = {fake_prob}")
     mock_config = SimpleNamespace(
         neuron=SimpleNamespace(
             prompt_type="annotation",
             sample_size=10,
             vpermit_tao_limit=1000
         ),
-        wandb=SimpleNamespace(off=True)
+        wandb=SimpleNamespace(off=True),
+        fake_prob=fake_prob
     )
-
     mock_neuron = MockValidator(mock_config)
-
     print("Calling forward with mock validator")
     try:
         await forward(self=mock_neuron)
