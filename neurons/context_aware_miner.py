da7863767dbd5b9c689e1ea693b82b6798438199
Benjamin <caliangben@gmail.com>
2024-08-13 05:14:24 -0500

    Fixed imports for context aware miner.

--- neurons/context_aware_miner.py
+++ neurons/context_aware_miner.py
@@ -29,17 +29,20 @@ import os
 import sys
 
 script_directory = os.path.dirname(os.path.realpath(__file__))
-detectors_path = os.path.join(script_directory, '../base_miner/UCF/detectors/')
-sys.path.append(detectors_path)
+base_ucf_path = os.path.join(script_directory, '../base_miner/UCF/')
+resolved_path = os.path.abspath(base_ucf_path)
+sys.path.append(resolved_path)
+predictor_path = os.path.join(base_ucf_path, 'preprocessing', 'dlib_tools',
+                              'shape_predictor_81_face_landmarks.dat')
 
-from base_miner.UCF.pretrained_ucf import UCF
+from pretrained_ucf import UCF
 from base_miner.NPR.networks.resnet import resnet50
 from bitmind.base.miner import BaseMinerNeuron
 from bitmind.protocol import ImageSynapse
 from bitmind.miner.predict import predict
 
-UCF_CONFIG_PATH = "./base_miner/UCF/config/ucf.yaml"
-UCF_WEIGHTS_PATH = "./base_miner/UCF/weights/"
+UCF_CONFIG_PATH = os.path.join(base_ucf_path, 'config', 'ucf.yaml')
+UCF_WEIGHTS_PATH = os.path.join(base_ucf_path, 'weights')
 UCF_CHECKPOINT_NAME = "ucf_best.pth"
 
 class Miner(BaseMinerNeuron):
@@ -71,7 +74,8 @@ class Miner(BaseMinerNeuron):
             # UCF for face detection
             self.face_model = UCF(config_path=UCF_CONFIG_PATH,
                              weights_dir=UCF_WEIGHTS_PATH,
-                             ucf_checkpoint_name=UCF_CHECKPOINT_NAME)
+                             ucf_checkpoint_name=UCF_CHECKPOINT_NAME,
+                             predictor_path=predictor_path)
 
             # NPR for general detection
             self.general_model = resnet50(num_classes=1)
