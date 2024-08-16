43207804eecaab5d9260e49357c4235fb77b19e7
Dylan Uys <dylan.uys@gmail.com>
2024-08-16 11:09:02 -0700

    adding .env file creation to install script

--- install_system_deps.sh
+++ install_system_deps.sh
@@ -3,5 +3,19 @@ sudo apt install python3-pip -y
 sudo apt install nano -y
 sudo apt install libgl1 -y
 sudo apt install npm -y
-sudo npm install pm2@latest -g 
+sudo npm install pm2@latest -g
+
+echo "MODEL_PATH=./mining_models/base.pth
+NETUID=34
+SUBTENSOR_NETWORK=finney
+WALLET_NAME=default
+WALLET_HOTKEY=default
+AXON_PORT=8091
+BLACKLIST_FORCE_VALIDATOR_PERMIT=True" > miner.env
+
+echo "NETUID=34
+SUBTENSOR_NETWORK=finney
+WALLET_NAME=default
+WALLET_HOTKEY=default
+AXON_PORT=8092" > validator.env
 
