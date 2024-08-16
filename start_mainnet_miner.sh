56d37a8ee504327c51b77068c1eb34c58df4f076
Dylan Uys <dylan.uys@gmail.com>
2024-08-16 10:09:58 -0700

    read btcli args from .env

--- start_mainnet_miner.sh
+++ start_mainnet_miner.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# Load environment variables from .env file
+set -a
+source .env
+set +a
+
 # Check if the process is already running
 if pm2 list | grep -q "bitmind_miner"; then
   echo "Process 'bitmind_miner' is already running. Deleting it..."
   pm2 delete bitmind_miner
 fi
 
-pm2 start python --name bitmind_miner -- neurons/miner.py --neuron.model_path ./mining_models/base.pth --netuid 34 --subtensor.network finney --wallet.name default --wallet.hotkey default --axon.port 8091 --blacklist.force_validator_permit True
+# Start the process with arguments from environment variables
+pm2 start python --name bitmind_miner -- neurons/miner.py \
+  --neuron.model_path $MODEL_PATH \
+  --netuid $NETUID \
+  --subtensor.network $SUBTENSOR_NETWORK \
+  --wallet.name $WALLET_NAME \
+  --wallet.hotkey $WALLET_HOTKEY \
+  --axon.port $AXON_PORT \
+  --blacklist.force_validator_permit $BLACKLIST_FORCE_VALIDATOR_PERMIT
--- start_mainnet_validator.sh
+++ start_mainnet_validator.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Load environment variables from .env file
+set -a
+source .env
+set +a
+
 # Check if the process is already running
 if pm2 list | grep -q "bitmind_validator"; then
   echo "Process 'bitmind_validator' is already running. Deleting it..."
   pm2 delete bitmind_validator
 fi
 
-pm2 start python --name bitmind_validator -- neurons/validator.py --netuid 34 --subtensor.network finney --wallet.name default --wallet.hotkey default --axon.port 8092
+# Start the process with arguments from environment variables
+pm2 start python --name bitmind_validator -- neurons/validator.py \
+  --netuid $NETUID \
+  --subtensor.network $SUBTENSOR_NETWORK \
+  --wallet.name $WALLET_NAME \
+  --wallet.hotkey $WALLET_HOTKEY \
+  --axon.port $VALIDATOR_AXON_PORT
