2e4fed1246bc6644439e3c21915e12d8eea01506
Dylan Uys <dylan.uys@gmail.com>
2024-08-16 11:21:02 -0700

    moving network (test/finney) into .env, reducing script redundancy

--- run_neuron.py
+++ run_neuron.py
@@ -15,7 +15,7 @@ def should_update_local(local_commit, remote_commit):
     return local_commit != remote_commit
 
 
-def run_auto_update_self_heal(neuron_type, network, auto_update, self_heal):
+def run_auto_update_self_heal(neuron_type, auto_update, self_heal):
     last_restart_time = time.time()
 
     while True:
@@ -44,14 +44,14 @@ def run_auto_update_self_heal(neuron_type, network, auto_update, self_heal):
                     time.sleep(20)
                     print("Finished running the autoupdate steps 😎")
                     print("Restarting neuron")
-                    os.system(f"./start_{network}_{neuron_type}.sh")
+                    os.system(f"./start_{neuron_type}.sh")
             else:
                 print("Repo is up-to-date.")
 
         if self_heal:
             # Check if it's time to restart the PM2 process
             if time.time() - last_restart_time >= RESTART_INTERVAL_HOURS * 3600:
-                os.system(f"./start_{network}_{neuron_type}.sh")
+                os.system(f"./start_{neuron_type}.sh")
                 last_restart_time = time.time()  # Reset the timer after the restart
 
 
@@ -59,24 +59,21 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Bittensor neuron run script with optional self-healing and auto-update.")
     parser.add_argument("--validator", action="store_true")
     parser.add_argument("--miner", action="store_true")
-    parser.add_argument("--network", default="finney", choices=["finney", "test"], help="finney or test")
     parser.add_argument("--no-self-heal", action="store_true", help="Disable the automatic restart of the PM2 process")
     parser.add_argument("--no-auto-update", action="store_true", help="Disable the automatic update of the local repository")
 
     args = parser.parse_args()
     if not (args.miner ^ args.validator):
-        print(f"Usage: python {__file__}" + "--validator | --miner [--network {finney,test} --no-self-heal --no-auto-update]")
+        print(f"Usage: python {__file__}" + "--validator | --miner [--no-self-heal --no-auto-update]")
         sys.exit(1)
 
     neuron_type = 'miner' if args.miner else 'validator'
-    network = 'mainnet' if args.network == 'finney' else 'testnet'
 
-    os.system(f"./start_{network}_{neuron_type}.sh")
+    os.system(f"./start_{neuron_type}.sh")
 
     if not args.no_auto_update or not args.no_self_heal:
         run_auto_update_self_heal(
             neuron_type,
-            network,
             auto_update=not args.no_auto_update,
             self_heal=not args.no_self_heal)
 
--- start_testnet_validator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if the process is already running
-if pm2 list | grep -q "bitmind_validator"; then
-  echo "Process 'bitmind_validator' is already running. Deleting it..."
-  pm2 delete bitmind_validator
-fi
-
-pm2 start python --name bitmind_validator -- neurons/validator.py  --netuid 168 --subtensor.network test --wallet.name default --wallet.hotkey default --axon.port 8092
