31060c34027dde3098f469d4072ff04e84ce2230
Andrew <caliangandrew@gmail.com>
2024-09-12 08:05:26 +0000

    Changed unit tests to compare deepfake_detectors registry const and new registry

--- base_miner/deepfake_detectors/unit_tests/test_registry.py
+++ base_miner/deepfake_detectors/unit_tests/test_registry.py
@@ -2,31 +2,30 @@ import unittest
 import os
 import sys
 
+#Registry class located in the parent directory
 directory = os.path.dirname(os.path.abspath(__file__))
 parent_directory = os.path.dirname(directory)
 sys.path.append(parent_directory)
-
-from detector_registry import DETECTOR_REGISTRY
 
 # Unit test class to test DETECTOR_REGISTRY
 class TestDetectorRegistry(unittest.TestCase):
     
     def test_registry_contents(self):
+        from detector_registry import Registry
+        detector_registry = Registry()
         # Check if the registry has the expected keys (class names or custom names)
-        registered_keys = list(DETECTOR_REGISTRY.data.keys())
+        registered_keys = list(detector_registry.data.keys())
         
         # Print all the registered models
         print("Registered detectors:")
         for name in registered_keys:
-            print(f"Detector Name: {name}, Class: {DETECTOR_REGISTRY[name]}")
+            print(f"Detector Name: {name}, Class: {detector_registry[name]}")
         
         # Assert that all expected keys are present
         self.assertEqual(len(registered_keys), 0, "There should be no registered detectors.")
 
     def test_registry_contents_after_import(self):
-        from camo_detector import CAMODetector
-        from ucf_detector import UCFDetector
-        from npr_detector import NPRDetector
+        from base_miner.deepfake_detectors import DETECTOR_REGISTRY
         # Check if the registry has the expected keys (class names or custom names)
         registered_keys = list(DETECTOR_REGISTRY.data.keys())
         
