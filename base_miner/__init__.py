cc838660dca89fcc25163fd17037a468d462f2a3
Andrew <caliangandrew@gmail.com>
2024-09-16 17:58:02 +0000

    Fixed import typo

--- base_miner/__init__.py
+++ base_miner/__init__.py
@@ -1,3 +1,3 @@
-rom .registry import DETECTOR_REGISTRY, GATE_REGISTRY
+from .registry import DETECTOR_REGISTRY, GATE_REGISTRY
 from .deepfake_detectors import NPRDetector, UCFDetector, CAMODetector
 from .gating_mechanisms import FaceGate, GatingMechanism
