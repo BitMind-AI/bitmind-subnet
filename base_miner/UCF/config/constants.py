628bdb3c338b1cb9a20a119f882ecf3bd88d5975
Andrew <caliangandrew@gmail.com>
2024-09-17 19:13:38 +0000

    Removed redundant path const

--- base_miner/UCF/config/constants.py
+++ base_miner/UCF/config/constants.py
@@ -1,12 +1,11 @@
 import os
 
 # Path to the directory containing the constants.py file
-UCF_CONFIGS_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIGS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # The base directory for UCF-related files, i.e., UCF directory
 UCF_BASE_PATH = os.path.abspath(os.path.join(UCF_CONFIGS_DIR, ".."))  # Points to bitmind-subnet/base_miner/UCF/
 # Absolute paths for the required files and directories
-CONFIGS_DIR = os.path.join(UCF_BASE_PATH, "config/")
 CONFIG_PATH = os.path.join(CONFIGS_DIR, "ucf.yaml")  # Path to the ucf.yaml file
 WEIGHTS_DIR = os.path.join(UCF_BASE_PATH, "weights/") # Path to pretrained weights directory
 
