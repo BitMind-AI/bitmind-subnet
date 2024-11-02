b4cdd549a9dc4c063c745ec2260d281e392cf027
Dylan Uys <dylan.uys@gmail.com>
2024-11-02 18:16:50 +0000

    removing no-save flags from

--- base_miner/UCF/ddp_train.sh
+++ base_miner/UCF/ddp_train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup python3 -m torch.distributed.launch --nproc_per_node=2 train_detector.py --no-save_ckpt --no-save_feat --ddp > ucf_ddp.log 2>&1 &
+nohup python3 -m torch.distributed.launch --nproc_per_node=2 train_detector.py --ddp > ucf_ddp.log 2>&1 &
