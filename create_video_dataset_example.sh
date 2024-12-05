633f27925e34eb55f328813328a032a8be4235c0
Dylan Uys <dylan.uys@gmail.com>
2024-12-05 00:30:49 +0000

    update default num_videosg

--- create_video_dataset_example.sh
+++ create_video_dataset_example.sh
@@ -4,10 +4,11 @@
 # --frames_dir is where the extracted png frames will be stored
 # --dataset_dir is where the huggingface dataset (containing paths to frames) will be stored
 # once your dataset is created, you can add its local path to base_miner/config.py for training
-python create_video_dataset.py --input_dir ~/.cache/sn34/real/video \
+python base_miner/datasets/create_video_dataset.py --input_dir ~/.cache/sn34/real/video \
        --frames_dir ~/.cache/sn34/train_frames \
        --dataset_dir ~/.cache/sn34/train_dataset/real_frames \
-       --num_videos 50 \
+       --num_videos 500 \
        --frame_rate 5 \
        --max_frames 24 \
-       --dataset_name real_frames
+       --dataset_name real_frames \
+       --overwrite 
