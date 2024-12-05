5f849c668e3d875cc10a70e6adc9d79fefba3668
Dylan Uys <dylan.uys@gmail.com>
2024-12-05 13:23:35 -0800

    autoupdate

--- autoupdate_validator_steps.sh
+++ autoupdate_validator_steps.sh
@@ -5,6 +5,6 @@
 # THIS FILE ITSELF MAY CHANGE FROM UPDATE TO UPDATE, SO WE CAN DYNAMICALLY FIX ANY ISSUES
 
 echo $CONDA_PREFIX
-$CONDA_PREFIX/bin/pip install -e .
+./setup_env.sh
 $CONDA_PREFIX/bin/python bitmind/validator/verify_models.py
 echo "Autoupdate steps complete :)"
