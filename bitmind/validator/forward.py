13348ff872cfe6fa3f86564d3d572896c00362fb
Dylan Uys <dylan.uys@gmail.com>
2025-01-07 05:57:49 +0900

    simplifying forward flow

--- bitmind/validator/forward.py
+++ bitmind/validator/forward.py
@@ -31,6 +31,19 @@ from bitmind.utils.uids import get_random_uids
 from bitmind.validator.config import CHALLENGE_TYPE, MAINNET_UID, TARGET_IMAGE_SIZE
 from bitmind.validator.reward import get_rewards
 
+def determine_challenge_type(fake_prob=0.5):
+    modality = 'video' if np.random.rand() > 0.5 else 'image'
+    label = 0 if np.random.rand() > fake_prob else 1
+    task = None
+    if label == 1:
+        if modality == 'video':
+            task = 't2v'
+        elif modality == 'image':
+            # 20% chance to use i2i (in-painting)
+            task = 'i2i' if np.random.rand() < 0.2 else 't2i'
+
+    return label, modality, task
+
 
 async def forward(self):
     """
@@ -52,10 +65,10 @@ async def forward(self):
     challenge_metadata = {}  # for bookkeeping
     challenge = {}           # for querying miners
 
-    modality = 'video' if np.random.rand() > 0.5 else 'image'
-    label = 0 if np.random.rand() > self._fake_prob else 1
+    label, modality, source_model_task = determine_challenge_type()
     challenge_metadata['label'] = label
     challenge_metadata['modality'] = modality
+    challenge_metadata['source_model_task'] = source_model_task
 
     bt.logging.info(f"Sampling data from {modality} cache")
     cache = self.media_cache[CHALLENGE_TYPE[label]][modality]
@@ -67,12 +80,6 @@ async def forward(self):
         challenge = cache.sample(num_frames, min_fps=8, max_fps=30)
 
     elif modality == 'image':
-        if label == 1: 
-            # 20% chance to use i2i (in-painting)
-            synthetic_type = 'i2i' if np.random.rand() < 0.2 else 't2i'
-            challenge_metadata['synthetic_type'] = synthetic_type
-            cache = cache[synthetic_type]
-
         challenge = cache.sample()
 
     if challenge is None:
