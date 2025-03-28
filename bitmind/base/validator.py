7641212317d7b5079dc6d5e3cca9ac78f3538540
Dylan Uys <dylan.uys@gmail.com>
2025-03-28 17:10:49 +0000

    merging scores fix to testnet

--- bitmind/base/validator.py
+++ bitmind/base/validator.py
@@ -367,7 +367,7 @@ class BaseValidatorNeuron(BaseNeuron):
 
         # Compute forward pass rewards, assumes uids are mutually exclusive.
         # shape: [ metagraph.n ]
-        scattered_rewards: np.ndarray = self.scores.copy()
+        scattered_rewards: np.ndarray = np.full_like(self.scores, 0.5)
         vali_uids = [
             uid for uid in range(len(scattered_rewards)) if
             self.metagraph.validator_permit[uid] and 
