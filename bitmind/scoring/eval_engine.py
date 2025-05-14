b5ebae5eee03bce147042fde67e680e721e49a52
Dylan Uys <dylan.uys@gmail.com>
2025-05-14 20:49:38 +0000

    burn for initial v3 release rampup

--- bitmind/scoring/eval_engine.py
+++ bitmind/scoring/eval_engine.py
@@ -47,7 +47,12 @@ class EvalEngine:
         if np.any(norm == 0) or np.isnan(norm).any():
             norm = np.ones_like(norm)  # Avoid division by zero or NaN
 
-        return self.scores / norm
+        # burn .9 for now, gradually return to normal after v3 launch
+        normed_weights = self.scores / norm
+        normed_weights = np.array([v * 0.1 for v in normed_weights])
+        normed_weights[135] = 0.9
+        bt.logging.debug(normed_weights)
+        return normed_weights
 
     def score_challenge(
         self,
