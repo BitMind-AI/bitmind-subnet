08f816e73e6bc1da51a7a5af3ccfca7f225c6cae
Dylan Uys <dylan.uys@gmail.com>
2025-05-14 19:51:23 +0000

    pointing autoupdate at main by default

--- bitmind/autoupdater.py
+++ bitmind/autoupdater.py
@@ -1,6 +1,7 @@
 # The MIT License (MIT)
 # Copyright © 2023 Yuma Rao
 # Copyright © 2024 Manifold Labs
+# Copyright © 2025 BitMind
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation
