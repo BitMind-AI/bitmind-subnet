13a6062009ba09b4dd8db02fbcab469e55450f30
Dylan Uys <dylan.uys@gmail.com>
2025-05-19 06:41:20 +0000

    adding label and mediatype to testnet metadata

--- neurons/validator.py
+++ neurons/validator.py
@@ -219,7 +219,7 @@ class Validator(BaseNeuron):
                         self.config.neuron.miner_connect_timeout,
                         self.config.neuron.miner_sock_connect_timeout,
                         testnet_metadata=(
-                            media_sample["metadata"]
+                            {k: v for k, v in media_sample.items() if k != modality}
                             if self.config.netuid != MAINNET_UID
                             else {}
                         ),
@@ -412,6 +412,7 @@ class Validator(BaseNeuron):
                     "modality": modality,
                     "media_type": media_type,
                     "label": MediaType(media_type).int_value,
+                    "metadata": sample.get("metadata", {}),
                 }
             )
             return sample
