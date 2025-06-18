1d08e17e03b7b423744636b8dd45b367048c40ea
Dylan Uys <dylan.uys@gmail.com>
2025-06-18 23:34:17 +0000

    removing scraping from this release

--- bitmind/config.py
+++ bitmind/config.py
@@ -106,13 +106,6 @@ def add_validator_args(parser):
     )
 
     parser.add_argument(
-        "--media-scraping-interval",
-        type=int,
-        help="How often to unpack random media files, measured in 12 second blocks",
-        default=400,
-    )
-
-    parser.add_argument(
         "--challenge-interval",
         type=int,
         help="How often we set challenge miners, measured in 12 second blocks.",
--- neurons/validator.py
+++ neurons/validator.py
@@ -28,7 +28,6 @@ from bitmind.metagraph import (
     run_block_callback_thread,
 )
 from bitmind.scoring import EvalEngine
-from bitmind.scraping import MultiSiteScraper, GoogleScraper
 from bitmind.transforms import apply_random_augmentations
 from bitmind.types import (
     MediaType,
@@ -80,18 +79,6 @@ class Validator(BaseNeuron):
 
         self.eval_engine = EvalEngine(self.metagraph, self.config)
 
-        self.scraper = MultiSiteScraper(
-            output_dir=self.config.cache.base_dir,
-            scrapers=[
-                GoogleScraper(
-                    min_width=128,
-                    min_height=128,
-                    headless=True,
-                    media_type=MediaType.REAL
-                )
-            ],
-        )
-
         ## REGISTER BLOCK CALLBACKS
         self.block_callbacks.extend(
             [
@@ -100,7 +87,6 @@ class Validator(BaseNeuron):
                 self.send_challenge_to_miners_on_interval,
                 self.update_compressed_cache_on_interval,
                 self.update_media_cache_on_interval,
-                #self.scrape_new_media_on_interval,
                 self.start_new_wanbd_run_on_interval,
             ]
         )
@@ -322,30 +308,6 @@ class Validator(BaseNeuron):
             target=update_media_cache, daemon=True
         )
         self._media_cache_thread.start()
-
-    @on_block_interval("media_scraping_interval")
-    async def scrape_new_media_on_interval(self, block, retries=3):
-        bt.logging.info("Scraping new media")
-
-        for i in range(retries):
-            if i < retries - 1:
-                bt.logging.warning("Retrying")
-
-            results = await self.cache_system.sample("synthetic_image_sampler", 1)
-            if not results or results.get("count", 0) == 0:
-                bt.logging.warning("No metadata available for search query generation")
-                continue
-
-            image_sample = results["items"][0]
-            search_query = image_sample.get("metadata", {}).get("prompt", "").split(".")[0]
-            if not search_query:
-                bt.logging.warning(f"Prompt not found in {image_sample.get('path', 'unknown path')} for search query generation")
-                continue 
-            else:
-                self.scraper.download_images(search_query, limit_per_scraper=50)
-                return
-
-        bt.logging.error("Aborting media scraping. Either no metadata or no prompts are available.")
 
     @on_block_interval("epoch_length")
     async def set_weights_on_interval(self, block):
