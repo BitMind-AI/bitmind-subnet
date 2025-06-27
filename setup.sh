a2a0d0d4d9212529bc36fa35b5e9fedca531a07d
Dylan Uys <dylan.uys@gmail.com>
2025-06-27 19:58:10 +0000

    restart instead of reload

--- setup.sh
+++ setup.sh
@@ -45,9 +45,9 @@ pip install --use-pep517 -e . -r requirements-git.txt
 echo "Performing restart of PM2 processes..."
 emergency_restart() {
     echo "Performing emergency restart of PM2 processes..."
-    pm2 reload sn34-generator 2>/dev/null || echo "Failed to reload sn34-generator"
-    pm2 reload sn34-proxy 2>/dev/null || echo "Failed to reload sn34-proxy"
-    pm2 reload sn34-validator 2>/dev/null || echo "Failed to reload sn34-validator"
+    pm2 restart sn34-generator 2>/dev/null || echo "Failed to reload sn34-generator"
+    pm2 restart sn34-proxy 2>/dev/null || echo "Failed to reload sn34-proxy"
+    pm2 restart sn34-validator 2>/dev/null || echo "Failed to reload sn34-validator"
 }
 
 trap emergency_restart EXIT
