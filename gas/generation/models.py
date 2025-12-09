5a68156574b322e5595dc18a67ed8a80f28573fb
Dylan Uys <dylan.uys@gmail.com>
2025-12-08 21:25:29 -0800

    Fix: Remove broken import (#309)

--- gas/generation/models.py
+++ gas/generation/models.py
@@ -19,7 +19,6 @@ from diffusers import (
     WanPipeline,
     ChromaPipeline,
     DiffusionPipeline,
-    HiDreamImagePipeline,
     TextToVideoSDPipeline,
 )
 
@@ -28,8 +27,7 @@ from gas.generation.util.model import (
     load_hunyuanvideo_transformer,
     load_annimatediff_motion_adapter,
     load_autoencoder_kl_wan,
-    load_hidream_llm_text_encoder,
-    load_hidream_tokenizer,
+
     JanusWrapper,
 )
 from gas.types import ModelConfig, ModelTask
