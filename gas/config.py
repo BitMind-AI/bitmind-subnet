8fe749a4b43343f41c7456fc33f954704b703611
dylan <dylan@bitmind.ai>
2026-02-27 22:25:11 +0000

    increase gen challenge interval from 80 to 110 blocks

--- gas/config.py
+++ gas/config.py
@@ -214,7 +214,7 @@ def add_validator_args(parser):
         "--generator-challenge-interval",
         type=int,
         help="How often we send challenges to generative miners, measured in 12 second blocks.",
-        default=80,
+        default=110,
     )
 
     parser.add_argument(
